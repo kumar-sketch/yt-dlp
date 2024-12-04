c83adbaad14c8ab9e6b221b4bf3a6841da945a7a
kumar-sketch <122518472+kumar-sketch@users.noreply.github.com>
2024-12-04 13:10:44 +0530

    Update yt-dlp.cmd

--- yt-dlp.cmd
+++ yt-dlp.cmd
@@ -1,1 +1,1 @@
-@py -Werror -Xdev "%~dp0yt_dlp\__main__.py" %*
+yt-dlp -f best -o "%(playlist_index)s - %(title)s.%(ext)s" --yes-playlist "https://www.youtube.com/channel/UC1SjI6gr88-kiIwZuWIj-nw/videos"
